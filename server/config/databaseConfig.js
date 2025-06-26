12536d69c5eadf2b768125e245a845ba12196b8e
helenzel <helenzelalem044@gmail.com>
2025-06-26 11:24:33 +0100

    fix

--- server/config/databaseConfig.js
+++ server/config/databaseConfig.js
@@ -5,7 +5,7 @@ const dbconfig = {
   host: process.env.DB_HOST || "localhost",
   port: process.env.DB_PORT || "3306",
   user: process.env.DB_USER || "forum",
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASSWORD ,
   database: process.env.DB_NAME || "evangadiforumfinal",
   waitForConnections: true,
   connectionLimit: 10,
