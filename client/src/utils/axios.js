29f74f08b58850928770638e447d6e52f7a1d6f9
helenzel <helenzelalem044@gmail.com>
2025-06-26 14:27:00 +0100

    fix

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3200/api",
+  // baseURL: "http://localhost:3200/api",
+  baseURL: "https://evangadiforum.be.helenzelalem.com/api",
 });
 
 export default axiosInstance;
